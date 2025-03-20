351ec59d0382c0b1ba5d51f29382c4bbbcefa316
annusinha253 <annusinha2503@gmail.com>
2025-03-20 12:35:52 +0530

    uploader done

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+
+st.set_page_config(page_title="Dava Visualization with Streamlit",
+                   page_icon="😊"
+
+)
+st.title("Dava Visualization with Streamlit")
+with st.sidebar:
+   upload=st.file_uploader("upload csv")
+
+if upload is not None:
+    df=pd.read_csv(upload)
+    st.dataframe(df.head())
